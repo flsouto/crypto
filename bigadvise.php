efe017db3ac3c3db7f552cb0fbdf4e41846f4c5d
Fabio <ti@traca.com.br>
2018-02-01 16:27:04 -0200

    changes

--- bigadvise.php
+++ bigadvise.php
@@ -5,8 +5,14 @@ $highest_symb = "";
 $highest_val = -999;
 
 foreach(scandir(__DIR__.'/snaps') as $symbol){
+    if(substr($symbol,-3)!='BTC'){
+        continue;
+    }
     $adv = get_advice($symbol);
     if(empty($adv['profit'])){
+        continue;
+    }
+    if($adv['score'] < 0){
         continue;
     }
     if($adv['profit'] > $highest_val){
